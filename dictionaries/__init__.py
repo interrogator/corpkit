69b7617d5e23b31d0df9f33446d26c868a142c41
Daniel McDonald <mcddjx@gmail.com>
2015-04-03 14:57:58 +1100

    process types added to init
    Former-commit-id: cc48256d0a5dd2d2ee76d3b5c14c07655d9b5102

--- dictionaries/__init__.py
+++ dictionaries/__init__.py
@@ -4,3 +4,5 @@ import dictionaries
 path_to_corpkit = os.path.dirname(corpkit.__file__)
 thepath, corpkitname = os.path.split(path_to_corpkit)
 os.environ["PATH"] += os.pathsep + path_to_corpkit + os.pathsep + os.path.join(thepath, 'dictionaries')
+
+from process_types import processes
