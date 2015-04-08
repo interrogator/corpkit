5cc0ab191d96f012598f1cc9cbc0c4466c68325a
Daniel McDonald <mcddjx@gmail.com>
2015-04-08 17:33:51 +1000

    adding building resources as they're written
    Former-commit-id: 6e5b0d40150eac1887a928a94a8b6ca13786339f

--- corpkit/build.py
+++ corpkit/build.py
@@ -2,54 +2,6 @@
 #   Building parsed corpora for discourse analysis
 #   Author: Daniel McDonald
 
-def correctspelling(path, newpath):
-    """Feed this function an unstructured corpus and get a version with corrected spelling"""
-    import enchant
-    import codecs
-    import os
-    subdirs = [d for d in os.listdir(path) if os.path.isdir(d)]
-    for subdir in subdirs:
-        txtFiles = [f for f in os.listdir(os.path.join(path,subdir)) if f.endswith(".txt")]
-        print 'Doing ' + subdir + ' ...'
-        for txtFile in txtFiles: 
-            d = enchant.Dict("en_UK")
-            try:
-                f = codecs.open(os.path.join(path,subdir,txtFile), "r", "utf-8")
-            except IOError:
-                print "Error reading the file, right filepath?"
-                return
-            textdata = f.read()
-            textdata = unicode(textdata, 'utf-8')
-            mispelled = [] # empty list. Gonna put mispelled words in here
-            words = textdata.split()
-            for word in words:
-                # if spell check failed and the word is also not in
-                # our mis-spelled list already, then add the word
-                if d.check(word) == False and word not in mispelled:
-                    mispelled.append(word)
-            # print mispelled
-            for mspellword in mispelled:
-                mspellword_withboundaries = '\b' + str(mspellword) + '\b'
-                #get suggestions
-                suggestions=d.suggest(mspellword)
-                #make sure we actually got some
-                if len(suggestions) > 0:
-                    # pick the first one
-                    picksuggestion=suggestions[0]
-                    picksuggestion_withboundaries = '\b' + str(picksuggestion) + '\b'
-
-                textdata = textdata.replace(mspellword_withboundaries,picksuggestion_withboundaries)
-            try:
-                if not os.path.exists(newpath):
-                    os.makedirs(newpath)
-                fo=open(os.path.join(newpath, txtFile), "w")
-            except IOError:
-                print "Error"
-                return 
-            fo.write(textdata.encode("UTF-8"))
-            fo.close()
-    return
-
 
 def dictmaker(path, dictname, dictpath = 'data/dictionaries'):
     """makes a pickle wordlist named dictname in dictpath"""
@@ -121,103 +73,190 @@ def dictmaker(path, dictname, dictpath = 'data/dictionaries'):
     time = strftime("%H:%M:%S", localtime())
     print time + ': Done! ' + dictname + ' created in ' + dictpath + '/'
 
-def downloader(url_list, new_path = 'downloaded', wait = 5):
+
+def get_urls(url, criteria = False, remove = True):
+    """Get a list of all urls within an html document"""
+    from bs4 import BeautifulSoup
+    configure_ipython_beautifulsoup(show_html=True, show_css=True, show_js=False)
+    soup = p(url)
+    urls = []
+    if criteria:
+        import re
+        regex = re.compile(criteria)
+    for link in soup.find_all('a'):
+        a_url = link.get('href')
+        if a_url.startswith('#'):
+            continue
+        if a_url.startswith('/'):
+            a_url = url + a_url
+        if criteria:
+            if re.search(regex, a_url):
+                if not remove:
+                    urls.append(a_url)
+            else:
+                if remove:
+                    urls.append(a_url)
+        else:
+            urls.append(a_url)
+    urls.sort()
+    filtered_urls = filter(None, urls)
+    unique_urls = sorted(set(filtered_urls))
+    return unique_urls
+
+
+def downloader(url_list, new_path = 'html', wait = 5):
     """download a bunch of urls and store in a local folder"""
     import urllib
     import time
     import os
-        if not os.path.exists(new_path):
-            os.makedirs(new_path)
-    for url in url_list:
+    from time import localtime, strftime
+    from corpkit.progressbar import ProgressBar
+    thetime = strftime("%H:%M:%S", localtime())
+    print "\n%s: Attempting to download %d URLs with %d seconds wait-time ... \n" % (thetime, len(url_list), wait)
+    p = ProgressBar(len(urls))
+    if not os.path.exists(new_path):
+        os.makedirs(new_path)
+    paths = []
+    for index, url in enumerate(url_list):
+        p.animate(index)
         base = os.path.basename(url)
-        urllib.urlretrieve(url, os.path.join(new_path, base))
+        new_filename = os.path.join(new_path, base)
+        paths.append(new_filename)
+        urllib.urlretrieve(url, new_filename)
         time.sleep(wait)
-    num_downloaded = len(os.listdir(new_path))
-    print 'Done! %d files downloaded.' % num_downloaded
+    p.animate(len(url_list))
+    num_downloaded = len(paths)
+    thetime = strftime("%H:%M:%S", localtime())
+    print '\n\n%s: Done! %d files downloaded.' % (thetime, num_downloaded)
+    return paths
 
-def get_urls(url, criteria = False, remove = True):
-    """Get all urls in the html of url"""
-    configure_ipython_beautifulsoup(show_html=True, show_css=True, show_js=False)
-    
-    # do it as another function, so that it could be recursive...
-    def getter(one_url):
-        soup = p(one_url)
-        urls = []
-        if criteria:
-            import re
-            regex = re.compile(criteria)
-        for link in soup.find_all('a'):
-            a_url = link.get('href')
-            if criteria:
-                if re.match(regex, a_url):
-                    if remove:
-                        pass
-                    elif not remove:
-                        urls.append(a_url)
-                else:
-                    if remove:
-                        urls.append(a_url)
-                    elif not remove:
-                        pass
-            else:
-                urls.append(a_url)
-        urls.sort()
-        filtered_urls = filter(None, urls)
-        return filtered_urls
-    
-    url_list = getter(url)
-    return url_list
-
-def text_extractor(html, stopwords = 'English'):
-    """extract text from html/xml files"""
+def simple_text_extractor(html, stopwords = 'English'):
+    """extract text from html/xml files using justext"""
     import requests
     import justext
     import os
     import copy
     # if on hard disk:
-    if os.path.isfile(html):
-        f = open(html)
-        html_text = f.read()
-    # if it's a web address
-    elif html.startswith('http'):
-        response = requests.get(html)
-        html_text = response.content
-    # if it's already html text:
+    if type(html) != list:
+        html_files = [copy.deepcopy(html)]
     else:
-        html_text = copy.deepcopy(html)
-    paragraphs = justext.justext(html_text, justext.get_stoplist(stopwords))
-    text = []
-    for paragraph in paragraphs:
-        if not paragraph.is_boilerplate:
-            text.append(paragraph.text)
-    text = '\n'.join(text)
-    return text
+        html_files = copy.deepcopy(html)
+    output = []
+    for html in html_files:
+        if os.path.isfile(html):
+            f = open(html)
+            raw_html_text = f.read()
+        # if it's a web address
+        elif html.startswith('http'):
+            response = requests.get(html)
+            raw_html_text = response.content
+        # if it's already html text:
+        else:
+            raw_html_text = copy.deepcopy(html)
+        paragraphs = justext.justext(raw_html_text, justext.get_stoplist(stopwords))
+        text = []
+        for paragraph in paragraphs:
+            if not paragraph.is_boilerplate:
+                text.append(paragraph.text)
+        text = '\n'.join(text)
+        metadata = os.path.basename(html)
+        tup = (text, metadata)
+        output.append(tup)
+    return output
 
-def structure_corpus(path_to_files, new_corpus_name = 'structured_corpus'):
-    """structure a corpus in some kind of sequence"""
+
+
+
+def practice_run(path_to_html_file):
     import os
-    import shutil
-    if not os.path.isdir(path_to_files):
-        raise ValueError('Directory not found: %s' % path_to_files)
-    if not os.path.exists(new_corpus_name):
-        os.makedirs(new_corpus_name)
-    files = os.listdir(path_to_files)
-    for f in files:
-        filepath = os.path.join(path_to_files, f)
-        
-        subcorpus_name = 'what goes here?'
-        subcorpus_path = os.path.join(new_corpus_name, subcorpus_name)
-        if not os.path.exists(subcorpus_path):
-            os.makedirs(subcorpus_path)
-        shutil.copy(filepath, subcorpus_path)
-    print 'Done!'
+    import warnings
+    from bs4 import BeautifulSoup
+    if type(path_to_html_file) == list:
+        path_to_html_file = str(path_to_html_file[0])
+    f = open(path_to_html_file)
+    raw = f.read()
+    soup = BeautifulSoup(raw)
+    try:
+        text = get_text(soup)
+    except:
+        function_defined = False
+        from corpkit.build import simple_text_extractor
+        simple_text_extractor(path_to_html_file)
+    try:
+        metadata = get_metadata(soup)
+    except:
+        warnings.warn('get_metadata function not defined. Using filename as metadata.')
+        metadata = os.path.basename(path_to_html_file)
+    print 'text: %s\n\nmetadata: %s' %(text, metadata)
+
+
+def souper(corpus_path):
+    """The aim is to make a tuple of (text, metadata)"""
+    import os
+    from bs4 import BeautifulSoup
+    for root, dirs, files in os.walk(corpus_path, topdown=True):
+        for name in files:
+            filepath = os.path.join(root, name)
+            f = open(filepath)
+            raw = f.read()
+            soup = BeautifulSoup(raw)
+
+
+def correctspelling(path, newpath):
+    """Feed this function an unstructured corpus and get a version with corrected spelling"""
+    import enchant
+    import codecs
+    import os
+    subdirs = [d for d in os.listdir(path) if os.path.isdir(d)]
+    for subdir in subdirs:
+        txtFiles = [f for f in os.listdir(os.path.join(path,subdir)) if f.endswith(".txt")]
+        print 'Doing ' + subdir + ' ...'
+        for txtFile in txtFiles: 
+            d = enchant.Dict("en_UK")
+            try:
+                f = codecs.open(os.path.join(path,subdir,txtFile), "r", "utf-8")
+            except IOError:
+                print "Error reading the file, right filepath?"
+                return
+            textdata = f.read()
+            textdata = unicode(textdata, 'utf-8')
+            mispelled = [] # empty list. Gonna put mispelled words in here
+            words = textdata.split()
+            for word in words:
+                # if spell check failed and the word is also not in
+                # our mis-spelled list already, then add the word
+                if d.check(word) == False and word not in mispelled:
+                    mispelled.append(word)
+            # print mispelled
+            for mspellword in mispelled:
+                mspellword_withboundaries = '\b' + str(mspellword) + '\b'
+                #get suggestions
+                suggestions=d.suggest(mspellword)
+                #make sure we actually got some
+                if len(suggestions) > 0:
+                    # pick the first one
+                    picksuggestion=suggestions[0]
+                    picksuggestion_withboundaries = '\b' + str(picksuggestion) + '\b'
+
+                textdata = textdata.replace(mspellword_withboundaries,picksuggestion_withboundaries)
+            try:
+                if not os.path.exists(newpath):
+                    os.makedirs(newpath)
+                fo=open(os.path.join(newpath, txtFile), "w")
+            except IOError:
+                print "Error"
+                return 
+            fo.write(textdata.encode("UTF-8"))
+            fo.close()
+    return
+
 
 def stanford_parse(corpus_path):
     """Parse a directory (recursively) with the Stanford parser..."""
     import os
     import ast
     try:
-        from corenlp import *
+        from corenlp import StanfordCoreNLP
     except:
         raise ValueError("CoreNLP not installed.")
     path_part, corpus_name = os.path.split(corpus_path)
@@ -238,14 +277,72 @@ def stanford_parse(corpus_path):
                 plain_text = sent['text']
             subcorpus_path = os.path.join(new_corpus_path, subcorpus_name)
             if not os.path.exists(subcorpus_path):
+                os.makedirs(subcorpus_path)
+
+
+def structure_corpus(path_to_files, new_corpus_name = 'structured_corpus'):
+    """structure a corpus in some kind of sequence"""
+    import os
+    import shutil
+    base = os.path.basename(path_to_files)
+    new_corpus_name = 'structured_' + base
+    if not os.path.isdir(path_to_files):
+        raise ValueError('Directory not found: %s' % path_to_files)
+    if not os.path.exists(new_corpus_name):
+        os.makedirs(new_corpus_name)
+    files = os.listdir(path_to_files)
+    for f in files:
+        filepath = os.path.join(path_to_files, f)
+        
+        subcorpus_name = 'what goes here?'
+        subcorpus_path = os.path.join(new_corpus_name, subcorpus_name)
+        if not os.path.exists(subcorpus_path):
             os.makedirs(subcorpus_path)
-       
-    # make file list with os.walk
-    # 
-    # parse every file
-    # 
-    # connect old to new?
-    # 
-    # make metadata?
-    # 
+        shutil.copy(filepath, subcorpus_path)
+    print 'Done!'
+
+def edit_metadata():
+    return edited
+
+def stanford_parse(data, corpus_name = 'corpus'):
+    from time import localtime, strftime
+    thetime = strftime("%H:%M:%S", localtime())
+    print "\n%s: Initialising CoreNLP... \n" % thetime
+    import os
+    import ast
+    try:
+        from corenlp import StanfordCoreNLP
+    except:
+        raise ValueError("CoreNLP not installed.")
+    from corpkit.progressbar import ProgressBar
+    corenlp = StanfordCoreNLP()
+    if not os.path.exists(corpus_name):
+        os.makedirs(corpus_name)
+    p = ProgressBar(len(data))
+    for index, datum in enumerate(data):
+        p.animate(index)
+        text = datum[0]
+        metadata = datum[1]
+        number_of_zeroes = len(str(len(data))) - 1
+        filename = str(index).zfill(number_of_zeroes) + '.txt' 
+        file_data = []
+        parsed_text = ast.literal_eval(corenlp.parse(text))
+        trees = []
+        raw_texts = []
+        for index, sent in enumerate(parsed_text['sentences']):
+            syntax_tree = sent['parsetree']
+            plain_text = sent['text']
+            trees.append(syntax_tree)
+            raw_texts.append(plain_text)
+                    #subcorpus_path = os.path.join(new_corpus_path, subcorpus_name)
+        file_data = ['<raw>' + '\n'.join(raw_texts) + '\n</raw>', '<parse>' + '\n'.join(trees) + '\n</parse>', ]
+        if not os.path.exists(os.path.join(corpus_name, metadata)):
+            os.makedirs(os.path.join(corpus_name, metadata))
+        try:
+            fo=open(os.path.join(corpus_name, metadata, filename),"w")
+        except IOError:
+            print "Error writing file."
+        fo.write('\n'.join(file_data))
+        fo.close()
+    p.animate(len(data))
     print 'Done!'
--- orientation/corpus_building.py
+++ orientation/corpus_building.py
@@ -20,38 +20,117 @@ from corpkit import x, y, z
 # Getting raw data
 
 # <markdowncell>
-# The function below will get all URLs listed on a webpage. You can use a `criteria` regex and `remove` to get 
+# The function below will get all URLs listed on a webpage. You can use a `criteria` regex and `remove` to remove/keep good URLs.
+
 # <codecell>
-urls = get_urls(url, criteria = False, remove = True):
+% load_ext soup
+urls = get_urls('http://www.theage.com.au', r'theage.*political-news', remove = False)
+print urls
 
 # <markdowncell>
-# You could, of course, do this recursively...
+# You could, of course, do this recursively. It would probably be wise to use `criteria` to restrict the domain:
 
 # <codecell>
 bigger_list = []
 for index, url in enumerate(urls):
     bigger_list.append(url)
     print 'Doing %d/%d...' % (index + 1, len(urls))
-    more_urls = get_urls(url)
+    more_urls = get_urls(url, r'www\.theage\.com\.au', remove = False)
     for u in more_urls:
         bigger_list.append(u)
-print len(bigger_list)
+# we only want unique entries:
+unique_urls = sorted(set(bigger_list))
+print '%d unique URLs!' len(unique_urls)
 
 # <headingcell level=2>
 # Downloading files
 
 # <markdowncell>
-# So long as space isn't an issue, it's probably better to download the HTML, as the page could vanish one day. Also, one of `corpkit`'s cool features is connecting HTML to concordance lines. This makes that much easier.
+# So long as hard disk space isn't an issue, it's better to download the HTML, as the page could vanish one day. Also, one of `corpkit`'s cool features is connecting HTML to concordance lines, which at present works with downloaded HTML only.
+
+# It's bad form to download a lot of URLs without a little but of wait time. Here, it's set to five, which is usually polite enough.
 
 # <codecell>
-downloader(urls, new_path = 'downloaded', wait = 5)
+# we won't use our bigger list...
+downloader(urls, new_path = 'html', wait = 5)
 
 # <markdowncell>
-# You'll now have a 'downloaded' folder in the current directory.
+# When it's done, there will be an 'html' folder in the current directory.
 
 # <headingcell level=2>
-# Getting text from files
+# Making a corpus from the HTML
 
+# <markdowncell>
+# Now that we have the HTML files, we can build a corpus.
+
+# To take advantage of all of `corpkit`'s features, we will build a corpus where each file contains:
+
+# 1. The raw text we're interested in
+# 2. The spelling corrected version
+# 2. The annotated versions of the spelling corrected version
+# 3. The original HTML, so that we can reconstitute the text's original context.
+
+# The other thing we'll do is structure the corpus by a metadata attribute. Rather than having a huge set of text files, we'll have a corpus with subcorpora (a folder with subfolders). `corpkit` will then be able to run over each subcorpus with the same query and plot results.
+
+# So, our workflow will be:
+
+# 1. Get the raw text of interest from the HTML and the metadata attribute we'll structure our corpus by
+# 2. Normalise the spelling of the texts, to aid parsing
+# 3. Parse the spelling corrected version with CoreNLP
+# 4. Make subcorpora, and put each text, annotations and html in the right spot
+
+# To make a well-structured corpus, some steps will require some user input, which may in turn mean that you need to write some Python from scratch. Below is a basic guide.
+
+# <headingcell level=3>
+# Get raw text and metadata
+
+# <markdowncell>
+# This is the most difficult part of the entire process, and it can't be totally automated, since what kind of text you want, and what metadata feature you want to use to structure the corpus, could be almost anything.
+
+# We're going to use Beautiful Soup, an HTML parser, to get what we want from our text. You may well need to use [its documentation](http://www.crummy.com/software/BeautifulSoup/bs4/doc/) to figure out how to extract the right things from your HTML.
+
+# `corpkit`'s functions use [ipython-beautifulsoup](https://github.com/Psycojoker/ipython-beautifulsoup), which very helpfully displays the raw and rendered HTML together.
+
+# What you need to do is define two functions, `get_text` and `get_metadata`:
+
+# <codecell>
+def get_text():
+	return text
+
+# <codecell>
+def get_metadata():
+	return metadata
+
+# <codecell>
+# attempt
+
+# <markdowncell>
+# What you want to end up with is a *tuple* with two parts: the text, and the metadata.
+
+# <codecell>
+# real
+
+# <headingcell level=4>
+# The simpler approach
+
+# <markdowncell>
+# If you're in a hurry to get text from the HTML, and aren't too fussed about exactly what part of the page it comes from, you can use `simple_text_extractor()`, which relies on [justext]():
+
+# <codecell>
+simple_text_extractor()
+
+# <headingcell level=3>
+# Normalise spelling
+
+# <codecell>
+correct_spelling()
+
+# <headingcell level=3>
+# Parse normalised texts
+
+
+# <headingcell level=3>
+# Make subcorpora
 # <markdowncell>
 # There are two major approaches. One is to use a heuristic approach to get anything in the HTML that looks like natural language. This option is most useful for people who are either very new to programming, or seeking to make larger, more general corpora. The second option is to traverse the HTML, and pull out the exact things we want. This can't be fully automated, because it's very different for everyone.
 
