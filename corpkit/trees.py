dd90fc0440d78360a5b75decd3e4f775c9f851ff
Daniel McDonald <mcddjx@gmail.com>
2015-04-15 10:37:10 +1000

    added searchtree and parsetree (untested)
    Former-commit-id: f4e2e0836e1bc7e4509c8ccf2091902b0966f537

---
+++ corpkit/trees.py
@@ -0,0 +1,66 @@
+# so far pretty much untested
+
+def searchtree(tree, query):
+    "Searches a tree with Tregex and returns matching terminals"
+    import os
+    try:
+        get_ipython().getoutput()
+    except TypeError:
+        have_ipython = True
+    except NameError:
+        import subprocess
+        have_ipython = False
+    fo = open('tree.tmp',"w")
+    fo.write(tree + '\n')
+    fo.close()
+    if have_ipython:
+        tregex_command = 'tregex.sh -o -t \'%s\' tree.tmp 2>/dev/null' % query
+        result_with_blank = get_ipython().getoutput(tregex_command)
+        result = [r for r in result_with_blank if r]
+    else:
+        tregex_command = ["tregex.sh", "-o", "-t", '%s' % query, "tree.tmp"]
+        FNULL = open(os.devnull, 'w')
+        result = subprocess.check_output(tregex_command, stderr=FNULL)
+        result = os.linesep.join([s for s in result.splitlines() if s]).split('\n')
+    tregex_command = 'sh ./tregex.sh -o -t \'' + query + '\' tmp.tree 2>/dev/null'
+    os.remove("tmp.tree")
+    return result
+
+def quicktree(sentence):
+    """Parse a sentence and return a visual representation in IPython"""
+    import os
+    from nltk import Tree
+    from nltk.draw.util import CanvasFrame
+    from nltk.draw import TreeWidget
+    try:
+        from stat_parser import Parser
+    except:
+        raise ValueError('PyStatParser not found.')
+    try:
+        from IPython.display import display
+        from IPython.display import Image
+    except:
+        pass
+    try:
+        get_ipython().getoutput()
+    except TypeError:
+        have_ipython = True
+    except NameError:
+        import subprocess
+        have_ipython = False
+    parser = Parser()
+    parsed = parser.parse(sentence)
+    cf = CanvasFrame()
+    tc = TreeWidget(cf.canvas(),parsed)
+    cf.add_widget(tc,10,10) # (10,10) offsets
+    cf.print_to_file('tree.ps')
+    cf.destroy()
+    if have_ipython:
+        tregex_command = 'convert tree.ps tree.png'
+        result = get_ipython().getoutput(tregex_command)
+    else:
+        tregex_command = ["convert", "tree.ps", "tree.png"]
+        result = subprocess.check_output(tregex_command)    
+    os.remove("tree.ps")
+    return Image(filename='tree.png')
+    os.remove("tree.png")
