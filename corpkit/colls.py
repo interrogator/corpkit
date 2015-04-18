7087b7d0e101c7657b780969aaa95399b0339353
Daniel McDonald <mcddjx@gmail.com>
2015-04-18 17:48:18 +1000

    unicode handling
    Former-commit-id: ce4c525a393b8922273e27af9b8152c337b5d208

--- corpkit/colls.py
+++ corpkit/colls.py
@@ -1,10 +1,9 @@
 
 def collocates(data, nbest = 30, window = 5):
-    """Feed this a csv file generated with conc() and get its collocations"""
+    """Feed this data and get its collocations"""
     import nltk
     from nltk import collocations
     from nltk.collocations import BigramCollocationFinder
-    import re
     import os
     import time
     from time import localtime, strftime
@@ -14,14 +13,16 @@ def collocates(data, nbest = 30, window = 5):
         from IPython.display import display, clear_output
     except ImportError:
         pass
-
     on_cloud = check_dit()
     # turn all sentences into long string
     time = strftime("%H:%M:%S", localtime())
     #if noprint is False:
     print "\n%s: Generating %d collocates ... \n" % (time, nbest)
     good = datareader(data, on_cloud = on_cloud)
-    good = unicode(good.lower(), 'utf-8', errors = 'ignore')
+    if type(good) != unicode:
+        good = unicode(good.lower(), 'utf-8', errors = 'ignore')
+    else:
+        good = good.lower()
     sent_tokenizer=nltk.data.load('tokenizers/punkt/english.pickle')
     sents = sent_tokenizer.tokenize(good)
     tokenized_sents = [nltk.word_tokenize(i) for i in sents]
@@ -34,14 +35,11 @@ def collocates(data, nbest = 30, window = 5):
             allwords.append(word)
     bigram_measures = nltk.collocations.BigramAssocMeasures()
     finder = BigramCollocationFinder.from_words(allwords, window_size=window)
+    # should be consistent in stopwords
     ignored_words = nltk.corpus.stopwords.words('english')
-    # anything containing letter or number
-    regex = r'[A-Za-z0-9]'
-    # the n't token
-    nonot = r'n\'t'
-    # lots of conditions!
     finder.apply_word_filter(lambda w: len(w) < 2 or w.lower() \
-        in ignored_words or not re.match(regex, w) or re.match(nonot, w))
+        in ignored_words or not w.isalnum())
+    #unncessary?:
     finder.apply_freq_filter(2)
     results = sorted(finder.nbest(bigram_measures.raw_freq, nbest))
     listversion = []
