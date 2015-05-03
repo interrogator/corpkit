8f2a04b0615a37b9c2a2987a53a21d722ace2efc
Daniel McDonald <mcddjx@gmail.com>
2015-05-04 08:27:30 +1000

    fixed built in queries
    Former-commit-id: 2e052538b32a8a8f0398ba541719b778c97483bb [formerly 9a455a2be41c2813887bf4d4e852acada8a55568] [formerly 9a455a2be41c2813887bf4d4e852acada8a55568 [formerly 9a455a2be41c2813887bf4d4e852acada8a55568 [formerly 6162420eceb680cf83c977890e01dcffe5f17c3d]]]
    Former-commit-id: 0e4c62c09102db20632c8241b93712c6a2be754c
    Former-commit-id: 29f67ccde658527f8e06504e3cc9261fbaf43104 [formerly c07ceb5db4695355751eccfaf8d3a8ceafb00ffa]
    Former-commit-id: 1e05cc9397f6123a893abce992e5932e3490dfe4

--- corpkit/query.py
+++ corpkit/query.py
@@ -340,20 +340,18 @@ def interrogator(path, options, query,
     # check if pythontex is being used:
     # have_python_tex = check_pythontex()
 
-
-
     # if query is a special query, convert it:
-    if query == 'any_word' or u'any_word' or r'any_word':
+    if 'any_word' in query:
         query = r'/.?[A-Za-z0-9].?/ !< __'
-    if query == 'subjects':
+    if 'subjects' in query:
         query = r'__ >># @NP'
-    if query == 'processes':
+    if 'processes' in query:
         query = r'/VB.?/ >># ( VP >+(VP) (VP !> VP $ NP))'
-    if query == 'modals':
+    if 'modals' in query:
         query = r'MD'
-    if query == 'participants':
+    if 'participants' in query:
         query = r'/(NN|PRP|JJ).?/ >># (/(NP|ADJP) $ VP | > VP)'
-    if query == 'entities':
+    if 'entities' in query:
         query = r'NP <# NNP'
         titlefilter = True
 
