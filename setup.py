e6dc875dece5bd94c3d0cedae9f928110687acc2
Daniel McDonald <mcddjx@gmail.com>
2015-06-05 18:16:08 +0800

    new version
    Former-commit-id: 9c850d6da0546cc8ed3010a3da6ac0144709d1a8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='corpkit',
-      version='0.31',
+      version='0.32',
       description='A toolkit for working with parsed corpora',
       url='http://github.com/interrogator/corpkit',
       author='Daniel McDonald',
