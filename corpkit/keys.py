a73c2cdeebece6abc73921bb0a6052e5fecd039e
Daniel McDonald <mcddjx@gmail.com>
2015-06-21 09:14:05 +0800

    many upgrades to keywords, ngrams, inc lemmatisation, jcw
    Former-commit-id: 8f2e96119f28d6c71ac186c1ed4f3df49e02dc2f

--- corpkit/keys.py
+++ corpkit/keys.py
@@ -16,25 +16,15 @@ def keywords(data,
     import re
     import time
     from time import localtime, strftime
+    from dictionaries.stopwords import stopwords as my_stopwords
 
     try:
         from IPython.display import display, clear_output
     except ImportError:
         pass
-    try:
-        get_ipython().getoutput()
-    except TypeError:
-        have_ipython = True
-    except NameError:
-        import subprocess
-        have_ipython = False
 
-    from corpkit.keys import spindle_ngrams, keywords_and_ngrams
+    from corpkit.keys import keywords_and_ngrams
     from corpkit.other import datareader
-    from corpkit.tests import check_dit
-
-    if n  == 'all':
-        n = 99999
 
     if not dictionary.endswith('.p'):
         dictionary = dictionary + '.p'
@@ -43,16 +33,32 @@ def keywords(data,
     time = strftime("%H:%M:%S", localtime())
     if printstatus:
         print "\n%s: Generating keywords ...\n" % time
+    
+    # get list of words
     good = datareader(data, **kwargs)
-    keywords = keywords_and_ngrams(good, dictionary = dictionary, show = 'keywords', **kwargs)
+    
+    # get keywords as list of tuples
+    kwds = keywords_and_ngrams(good, dictionary = dictionary, show = 'keywords', **kwargs)
+
+    # remove bad stuff from result
+    regex_nonword_filter = re.compile("[A-Za-z-\']")
+    kwds = [i for i in kwds if re.search(regex_nonword_filter, i[0]) and i[0] not in my_stopwords] 
+
+    # turn into series
     import pandas as pd
-    out = pd.Series([s for k, s in keywords], index = [k for k, s in keywords])
+    out = pd.Series([s for k, s in kwds], index = [k for k, s in kwds])
     out.name = 'keywords'
+    
+    # print and return
     if clear:
         clear_output()
     if printstatus:
         time = strftime("%H:%M:%S", localtime())
         print '%s: Done! %d results.\n' % (time, len(list(out.index)))
+
+    if n  == 'all':
+        n = len(out)
+
     return out[:n]
 
 def ngrams(data,
@@ -73,22 +79,15 @@ def ngrams(data,
     import re
     import time
     from time import localtime, strftime
+    from dictionaries.stopwords import stopwords as my_stopwords
 
     try:
         from IPython.display import display, clear_output
     except ImportError:
         pass
-    try:
-        get_ipython().getoutput()
-    except TypeError:
-        have_ipython = True
-    except NameError:
-        import subprocess
-        have_ipython = False
 
-    from corpkit.keys import spindle_ngrams, keywords_and_ngrams
+    from corpkit.keys import keywords_and_ngrams
     from corpkit.other import datareader
-    from corpkit.tests import check_dit
 
     if n == 'all':
         n = 99999
@@ -97,24 +96,26 @@ def ngrams(data,
     if printstatus:
         print "\n%s: Generating ngrams... \n" % time
     good = datareader(data, **kwargs)
-    if 'lemmatise' in kwargs:
-        if kwargs['lemmatise'] is True:
-            good = ' '.join(good)
-    good = datareader(data, **kwargs)
     ngrams = keywords_and_ngrams(good, dictionary = dictionary, show = 'ngrams', **kwargs)
+
+    regex_nonword_filter = re.compile("[A-Za-z-\']")
+    ngrams = [i for i in ngrams if re.search(regex_nonword_filter, i[0]) and i[0] not in my_stopwords] 
+
     import pandas as pd
     out = pd.Series([s for k, s in ngrams], index = [k for k, s in ngrams])
     out.name = 'ngrams'
+
+    # print and return
     if clear:
         clear_output()
+    if printstatus:
+        time = strftime("%H:%M:%S", localtime())
+        print '%s: Done! %d results.\n' % (time, len(list(out.index)))
+
+    if n  == 'all':
+        n = len(out)
+
     return out[:n]
-
-
-
-# from Spindle: just altered dictionary stuff and number to show
-def spindle_ngrams(words, n=2, nngram = 'all'):
-    return (tuple(words[i:i+n]) for i in range(0, len(words) - (n - 1)))
-
 
 # from Spindle
 def keywords_and_ngrams(input, thresholdBigrams=2, 
@@ -122,54 +123,55 @@ def keywords_and_ngrams(input, thresholdBigrams=2,
                         **kwargs):
     from collections import defaultdict
     import math
+    import re
     import json
     import os
-    from dictionaries.stopwords import stopwords as my_stopwords
     import cPickle as pickle
+    from dictionaries.stopwords import stopwords as my_stopwords
+
+
+    # from Spindle: just altered dictionary stuff and number to show
+    def spindle_ngrams(words, n=2, nngram = 'all'):
+        return (tuple(words[i:i+n]) for i in range(0, len(words) - (n - 1)))
+
     if not dictionary.endswith('.p'):
         dictionary = dictionary + '.p'
     try:
-        fdist_dictfile = pickle.load( open( dictionary, "rb" ) )
+        ref_corp_dict = pickle.load( open( dictionary, "rb" ) )
     except IOError:
         try:
-            fdist_dictfile = pickle.load( open( os.path.join('data/dictionaries', dictionary), "rb" ) )
+            ref_corp_dict = pickle.load( open( os.path.join('data/dictionaries', dictionary), "rb" ) )
         except IOError:
             try:
                 import corpkit
                 path_to_corpkit = os.path.dirname(corpkit.__file__)
                 thepath, corpkitname = os.path.split(path_to_corpkit)
                 dictionaries_path = os.path.join(thepath, 'dictionaries')
-                fdist_dictfile = pickle.load( open( os.path.join(dictionaries_path, dictionary), "rb" ) )
+                ref_corp_dict = pickle.load( open( os.path.join(dictionaries_path, dictionary), "rb" ) )
             except IOError:
                 raise IOError('Could not find %s in current directory, data/dictionaries or %s' % (dictionary, dictionaries_path))
     
     # Total number of words in reference corpus
-    dictsum = sum(fdist_dictfile.itervalues())
-    fdistText = defaultdict(int)
-    sumText = 0
-    listWords = []
-    listSentences = []
-    
-    if isinstance(input,str):
-        listSentences = [input]
-    else:
-        listSentences = input
-    
-    for line in listSentences:
-        for w in line.split():
-            w = w.lower()
-            listWords.append(w)
-            sumText = sumText+1
-            if w not in my_stopwords and w.isalpha() and len(w) > 2:
-                fdistText[w] = fdistText[w]+1
+    dictsum = sum(ref_corp_dict.itervalues())
+
+    from collections import Counter
+    counter_target_corpus = Counter(input)
+
+    # target corpus word count
+    sumText = sum(counter_target_corpus.values())
 
     dicLL = {}
 
-    for k, v in fdistText.items():
-        a = fdist_dictfile.get(k)
-        b = fdistText.get(k)
+    for k, b in counter_target_corpus.items():
+        # to handle non-Counter dicts
+        try:
+            a = ref_corp_dict[k]
+        except KeyError:
+            a = 0
+        #b = counter_target_corpus.get(k)
         c = dictsum
         d = sumText
+        # this is needed 
         if a == None:
             a = 0
         if b == None:
@@ -183,19 +185,24 @@ def keywords_and_ngrams(input, thresholdBigrams=2,
         dicLL[k] = float(2* ((a*logaE1)+(b*math.log(b/E2))))
 
     sortedLL = sorted(dicLL, key=dicLL.__getitem__, reverse=True)
-    listKeywords = [(unicode(k, 'utf-8', errors = 'ignore'), dicLL[k]) for k in sortedLL if k.isalpha()]
+    listKeywords = [(k, dicLL[k]) for k in sortedLL]
 
+    # list of keywords
     keywords = [keyw[0] for keyw in listKeywords]
-    counts = defaultdict(int)
-    for ng in spindle_ngrams(listWords, 2):
-        counts[ng] += 1
+    
+    ngms = spindle_ngrams(input, 2)
+    counter_ngrams = Counter(ngms)
 
     if show == 'ngrams':
+        regex_nonword_filter = re.compile("[A-Za-z]")
+        # this doesn't do much now, because everything is in keywords!
         listBigrams = []
-        for c, ng in sorted(((c, ng) for ng, c in counts.iteritems()), reverse=True):
+        for c, ng in sorted(((c, ng) for ng, c in counter_ngrams.items()), reverse=True):
             w0 = ng[0]
             w1 = ng[1]
-            if w0 in keywords and w1 in keywords and c>thresholdBigrams:
+            if (all([i not in my_stopwords for i in ng]) 
+                and all([re.search(regex_nonword_filter, i) for i in ng]) 
+                and c>thresholdBigrams):
                 listBigrams.append((' '.join(ng), c))
 
     if show == 'keywords':
