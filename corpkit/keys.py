085f1770da16673fd6b53207ec7d7926accae895
Daniel McDonald <mcddjx@gmail.com>
2015-06-22 11:27:05 +0800

    user input for incorrect dictionaries
    Former-commit-id: 7f90ffffc36cffb2015bb53a4c4f90f24912ca23

--- corpkit/keys.py
+++ corpkit/keys.py
@@ -23,11 +23,11 @@ def keywords(data,
     except ImportError:
         pass
 
-    from corpkit.keys import keywords_and_ngrams
+    from corpkit.keys import keywords_and_ngrams, get_ref_corpus
     from corpkit.other import datareader
 
-    if not dictionary.endswith('.p'):
-        dictionary = dictionary + '.p'
+
+    loaded_ref_corpus = get_ref_corpus(dictionary)
 
     # turn all sentences into long string
     time = strftime("%H:%M:%S", localtime())
@@ -42,11 +42,12 @@ def keywords(data,
     good = [i for i in good if re.search(regex_nonword_filter, i)] 
     
     # get keywords as list of tuples
-    kwds = keywords_and_ngrams(good, dictionary = dictionary, show = 'keywords', **kwargs)
+    kwds = keywords_and_ngrams(good, dictionary = loaded_ref_corpus, show = 'keywords', **kwargs)
 
     # turn into series
     import pandas as pd
     out = pd.Series([s for k, s in kwds], index = [k for k, s in kwds])
+    pd.set_option('display.float_format', lambda x: '%.2f' % x)
     out.name = 'keywords'
     
     # print and return
@@ -86,8 +87,10 @@ def ngrams(data,
     except ImportError:
         pass
 
-    from corpkit.keys import keywords_and_ngrams
+    from corpkit.keys import keywords_and_ngrams, get_ref_corpus
     from corpkit.other import datareader
+
+    loaded_ref_corpus = get_ref_corpus(dictionary)
 
     if n == 'all':
         n = 99999
@@ -134,26 +137,9 @@ def keywords_and_ngrams(input, thresholdBigrams=2,
     # from Spindle: just altered dictionary stuff and number to show
     def spindle_ngrams(words, n=2, nngram = 'all'):
         return (tuple(words[i:i+n]) for i in range(0, len(words) - (n - 1)))
-
-    if not dictionary.endswith('.p'):
-        dictionary = dictionary + '.p'
-    try:
-        ref_corp_dict = pickle.load( open( dictionary, "rb" ) )
-    except IOError:
-        try:
-            ref_corp_dict = pickle.load( open( os.path.join('data/dictionaries', dictionary), "rb" ) )
-        except IOError:
-            try:
-                import corpkit
-                path_to_corpkit = os.path.dirname(corpkit.__file__)
-                thepath, corpkitname = os.path.split(path_to_corpkit)
-                dictionaries_path = os.path.join(thepath, 'dictionaries')
-                ref_corp_dict = pickle.load( open( os.path.join(dictionaries_path, dictionary), "rb" ) )
-            except IOError:
-                raise IOError('Could not find %s in current directory, data/dictionaries or %s' % (dictionary, dictionaries_path))
     
     # Total number of words in reference corpus
-    dictsum = sum(ref_corp_dict.itervalues())
+    dictsum = sum(dictionary.itervalues())
 
     from collections import Counter
     counter_target_corpus = Counter(input)
@@ -166,7 +152,7 @@ def keywords_and_ngrams(input, thresholdBigrams=2,
     for k, b in counter_target_corpus.items():
         # to handle non-Counter dicts
         try:
-            a = ref_corp_dict[k]
+            a = dictionary[k]
         except KeyError:
             a = 0
         #b = counter_target_corpus.get(k)
@@ -210,3 +196,55 @@ def keywords_and_ngrams(input, thresholdBigrams=2,
         return listKeywords
     elif show == 'ngrams':
         return listBigrams
+
+
+def get_ref_corpus(dictionary):
+    """Make sure we have a reference corpus"""
+    import sys
+    import os
+    import pickle
+    if not dictionary.endswith('.p'):
+        dictionary = dictionary + '.p'
+    dict_found = False
+    while not dict_found:
+        try:
+            ref_corp_dict = pickle.load( open( dictionary, "rb" ) )
+            dict_found = True
+            return ref_corp_dict
+        except IOError:
+            try:
+                ref_corp_dict = pickle.load( open( os.path.join('data/dictionaries', dictionary), "rb" ) )
+                dict_found = True
+                return ref_corp_dict
+            except IOError:
+                try:
+                    import corpkit
+                    path_to_corpkit = os.path.dirname(corpkit.__file__)
+                    thepath, corpkitname = os.path.split(path_to_corpkit)
+                    dictionaries_path = os.path.join(thepath, 'dictionaries')
+                    ref_corp_dict = pickle.load( open( os.path.join(dictionaries_path, dictionary), "rb" ) )
+                    dict_found = True
+                    return ref_corp_dict
+                except IOError:
+                        pass
+
+        dict_of_dicts = {}
+        d_for_print = []
+        
+        dicts = [f for f in os.listdir('data/dictionaries') if f.endswith('.p')]
+        for index, d in enumerate(dicts):
+            dict_of_dicts[index] = d
+            d_for_print.append('    % 2d) %s' % (index, d))
+        
+        d_for_print = '\n'.join(d_for_print) 
+
+        selection = raw_input("\nDictionary not found. Select an existing dictionary or exit or type 'exit' to quit.\n\n%s\n\nYour selection: " % d_for_print)
+        if selection.startswith('e'):
+            import sys
+            return
+        else:
+            try:
+                dictionary = dict_of_dicts[int(selection)]
+            except:
+                print '\nInput "%s" not recognised.' % dictionary
+
