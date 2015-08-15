5296af94e387f8214a635e7c9e7812771e885eac
Daniel McDonald <mcddjx@gmail.com>
2015-08-16 00:50:23 +0800

    new setup
    Former-commit-id: 4b0656b31bc7c310fdd8055cbf112b9322fb126d

--- setup.py
+++ setup.py
@@ -5,31 +5,17 @@ setup(name='corpkit',
       description='A toolkit for working with linguistic corpora',
       url='http://github.com/interrogator/corpkit',
       author='Daniel McDonald',
-      package_data={'corpkit': ['*.jar', 'corpkit/*.jar'],
-                    'corpkit': ['*.sh', 'corpkit/*.sh'],
-                    'corpkit': ['*.ipynb', 'corpkit/*.ipynb'],
+      package_data={'corpkit': ['*.jar', 'corpkit/*.jar', '*.sh', 'corpkit/*.sh', 
+                                '*.ipynb', 'corpkit/*.ipynb'],
                     'dictionaries': ['*.p', 'dictionaries/*.p']},
       author_email='mcdonaldd@unimelb.edu.au',
       license='MIT',
       packages=['corpkit', 'dictionaries'],
-      install_requires=[
-                        "matplotlib >= 1.4.3",
+      install_requires=["matplotlib >= 1.4.3",
                         "nltk >= 3.0.0",
                         "pandas >= 0.16.1",
                         "mpld3 >= 0.2", 
                         "beautifulsoup4>=4.3.2",
                         "pattern>=2.6",
-                        "tkintertable>=1.2"
-                        # add bs4, what else? ipython?
-                       ],
-      #extras_require = {
-      #                  'dependencies':  ["ReportLab>=1.2", "RXP"],
-      #                  'view': ["pandas>=0.15.2"],
-      #                 }
-      #entry_points = {
-      #                'corpkit': [
-      #                'rst2pdf = project_a.tools.pdfgen [PDF]',
-      #                'rst2html = project_a.tools.htmlgen',
-      #                           ],
-      #                }
-      zip_safe=False)
+                        "tkintertable>=1.2"],
+      zip_safe = False)
