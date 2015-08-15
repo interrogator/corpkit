8702178f00518d7272d642de3bf30aa9ea9ed814
Daniel McDonald <mcddjx@gmail.com>
2015-08-16 00:50:03 +0800

    simpler inits
    Former-commit-id: 1112b08ce848e3a96d20efc1f7d4f55dcf509b54 [formerly d19a80e99dc1c3d98c08b5278ab82c117999df12]
    Former-commit-id: f48ac04bc3fbdb6f0c42e61be122f57370aad174
    Former-commit-id: aaca090b26909707cba038a7777caac452b2390f [formerly 4a8899e6acb07d45fd89a78a4ca77dccac84dddb]
    Former-commit-id: 1c8eede8dd61ff043c5e091a794bfc339eb0e724

--- corpkit/__init__.py
+++ corpkit/__init__.py
@@ -1,104 +1,33 @@
-__all__ = ["interrogator",
-           "editor",
-           "plotter",
-           "conc",
-           "keywords",
-           "ngrams",
-           "collocates",
-           "quickview",
-           "concprinter",
-           "save_result",
-           "load_result",
-           "report_display",
-           "ipyconverter",
-           "conv",
-           "pytoipy",
-           "new_project",
-           "multiquery",
-           "interroplot",
-           "quicktree",
-           "searchtree",
-           "tregex_engine",
-           "load_all_results",
-           "dictmaker",
-           "texify",
-           "make_nltk_text",
-           "pmultiquery",
-           "as_regex",
-           "sim",
-           "show",
-           "dictmaker",
-           "get_urls",
-           "downloader",
-           "simple_text_extractor",
-           "practice_run",
-           "souper",
-           "correctspelling",
-           "stanford_parse",
-           "structure_corpus",
-           "edit_metadata",
-           "stanford_parse",
-           "download_cnlp",
-           "extract_cnlp",
-           "rename_duplicates",
-           "get_corpus_filepaths",
-           "check_jdk",
-           "parse_corpus",
-           "move_parsed_files",
-           "corenlp_exists"]
+#__all__ = ["interrogator",
+#           "editor",
+#           "plotter",
+#           "conc",
+#           "save_result",
+#           "quickview",
+#           "load_result",
+#           "load_all_results",
+#           "as_regex"]
+
+import sys
+import os
+import inspect
+
+corpath = inspect.getfile(inspect.currentframe())
+baspat = os.path.dirname(corpath)
+dicpath = os.path.join(baspat, 'dictionaries')
+for p in [corpath, baspat, dicpath]:
+    if p not in sys.path:
+        sys.path.append(p)
+    if p not in os.environ["PATH"].split(':'): 
+        os.environ["PATH"] += os.pathsep + p
 
 from interrogator import interrogator
 from editor import editor
 from plotter import plotter
 from conc import conc
-from keys import keywords
-from keys import ngrams
-from colls import collocates
-from plugins import HighlightLines, InteractiveLegendPlugin
-
-from other import quickview
-from other import concprinter
 from other import save_result
 from other import load_result
-from other import report_display
-from other import ipyconverter
-from other import conv
-from other import pytoipy
-from other import new_project
-from other import multiquery
-from other import interroplot
-from other import quicktree
-from other import searchtree
-from other import datareader
-from other import tregex_engine
 from other import load_all_results
-from other import texify
-from other import make_nltk_text
-from other import pmultiquery
+from other import quickview
 from other import as_regex
-from other import show
-from context import sim
-
-from build import download_cnlp
-from build import dictmaker
-from build import get_urls
-from build import downloader
-from build import simple_text_extractor
-from build import practice_run
-from build import souper
-from build import correctspelling
-from build import stanford_parse
-from build import structure_corpus
-from build import edit_metadata
-from build import extract_cnlp
-from build import get_corpus_filepaths
-from build import check_jdk
-from build import parse_corpus
-from build import move_parsed_files
-from build import corenlp_exists
-
-from dictionaries.process_types import processes
-from dictionaries.wordlists import wordlists
-from dictionaries.roles import roles
-
-
+from other import add_corpkit_to_path
--- dictionaries/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-import corpkit
-import dictionaries
-path_to_corpkit = os.path.dirname(corpkit.__file__)
-thepath, corpkitname = os.path.split(path_to_corpkit)
-os.environ["PATH"] += os.pathsep + path_to_corpkit + os.pathsep + os.path.join(thepath, 'dictionaries')
-
-from process_types import processes
