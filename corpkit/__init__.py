2bbaacb1e8941cd091c3b8e6f0c2915a344040d5
Daniel McDonald <mcddjx@gmail.com>
2015-08-16 23:06:44 +0800

    more default imports
    Former-commit-id: ab8b62cb5734004e8ffd89ff17d373a193dc7049

--- corpkit/__init__.py
+++ corpkit/__init__.py
@@ -6,11 +6,20 @@ __all__ = ["interrogator",
            "quickview",
            "load_result",
            "load_all_results",
-           "as_regex"]
+           "as_regex",
+           "new_project",
+           "download_cnlp", 
+            "extract_cnlp", 
+            "rename_duplicates", 
+            "get_corpus_filepaths", 
+            "check_jdk", 
+            "parse_corpus", 
+            "move_parsed_files", 
+            "corenlp_exists"]
 
-__version__ = "1.13"
-__author__    = "Daniel McDonald"
-__license__   = "MIT"
+__version__ = "1.17"
+__author__ = "Daniel McDonald"
+__license__ = "MIT"
 
 import sys
 import os
@@ -36,3 +45,12 @@ from other import load_result
 from other import load_all_results
 from other import quickview
 from other import as_regex
+from other import new_project
+from build import download_cnlp
+from build import extract_cnlp
+from build import rename_duplicates
+from build import get_corpus_filepaths
+from build import check_jdk
+from build import parse_corpus
+from build import move_parsed_files
+from build import corenlp_exists
