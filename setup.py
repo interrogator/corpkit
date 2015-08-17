6f4539567a55a26f649e6cdf77fb3658183a1814
Daniel McDonald <mcddjx@gmail.com>
2015-08-17 11:26:35 +0800

    update to 20
    Former-commit-id: 95533c190eb4ce99fa08fdd371b6ac6f0abed8cd [formerly 149b2555469a50d17be8ca11713514be8d53dffd]
    Former-commit-id: 6b4a82ade91b2965e6e4084ce7cfcf1abaab7a81
    Former-commit-id: f752bdb0355b19bbc3673a694c189ef586defee0
    Former-commit-id: fc875acb67f3c0787542ce5e6ebf2b59d4c1b182 [formerly 4e7e96eedc376fde90ebf8319155484085b42dd4]
    Former-commit-id: 9b1d3d904990f651b104039d2008791d55eed21e

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='corpkit',
-      version='1.19',
+      version='1.20',
       description='A toolkit for working with linguistic corpora',
       url='http://github.com/interrogator/corpkit',
       author='Daniel McDonald',
