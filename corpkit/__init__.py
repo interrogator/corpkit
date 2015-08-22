7c8594ec28effe1165a82a33b7e709033988576d
Daniel McDonald <mcddjx@gmail.com>
2015-08-23 00:12:48 +0800

    new version
    Former-commit-id: f21048d28ecc4d2ee0d64c1ad26c4f9c8731e761 [formerly 7081916cc5f47581199796cf232d804e958f12c4]
    Former-commit-id: 9914401bbcf0e69d4e46ebf0db49c814d4b9c59d
    Former-commit-id: 3cd884a6c3ae44935e7f416409a69ededff33a0d
    Former-commit-id: c364ef7a8bae5abf1d8e9145484d73166fc65a81 [formerly 2008577f33fd74b8ced7ff3b3fa6b85e53351579]
    Former-commit-id: 16e6847e87f47ad27e529873fbcabeba87efe0f0

--- corpkit/__init__.py
+++ corpkit/__init__.py
@@ -16,7 +16,7 @@ __all__ = ["interrogator",
            "move_parsed_files", 
             "corenlp_exists"]
 
-__version__ = "1.30"
+__version__ = "1.31"
 __author__ = "Daniel McDonald"
 __license__ = "MIT"
 
