e3e7fcaf7374f0288558433bfb85430acd477eea
Daniel McDonald <mcddjx@gmail.com>
2015-08-31 18:00:57 +0800

    dictionaries working, bugfixes
    Former-commit-id: 715217d3409512b5ef98b8989735a92a10bb52c9 [formerly 6133b87c135ba79137d98021e736ced56a471ba7]
    Former-commit-id: c8718f6f7eff7c8a230d7755034e27920ee88c6b
    Former-commit-id: 8c415639681d2f1734c95e26b7f782b99675b943
    Former-commit-id: 79f52158149997e76cd57b500348e0f3f7dba97a [formerly 64bc145902ba22947af4723aaa60ea07bd244829]
    Former-commit-id: 60f71476951caab6e3b457be8dfbf7aeada7b3c8

--- corpkit/__init__.py
+++ corpkit/__init__.py
@@ -16,7 +16,7 @@ __all__ = ["interrogator",
            "move_parsed_files", 
             "corenlp_exists"]
 
-__version__ = "1.42"
+__version__ = "1.43"
 __author__ = "Daniel McDonald"
 __license__ = "MIT"
 
