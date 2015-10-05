255c063074d69da66bd29790ef2899de64370f90
daniel <mcdonaldd@unimelb.edu.au>
2015-10-05 21:36:50 +0200

    getting setup.py working again
    Former-commit-id: ae3534f498664f74138e9b431ce4b6d23eb00b18
    Former-commit-id: 21e0f3f3a8178d8779ccce14fd7740d84e1a09ab [formerly 04b5cd4884c082e8d72b8257e4b51487cb469d1c]
    Former-commit-id: 3098862ba4ea58950b9d3481e68008b823d43018

--- setup.py
+++ setup.py
@@ -1,57 +1,41 @@
 from setuptools import setup, find_packages
 from setuptools.command.install import install
-from setuptools.command.develop import develop
-from setuptools.command.install import install as _install  
-from setuptools.command.develop import develop as _develop
 
-#def _post_install():  
-#
-#    def install_with_pip(package_name, package):
-#        import importlib
-#        try:
-#            importlib.import_module(package_name)
-#        except ImportError:
-#            import pip
-#            pip.main(['install', package])
-#
-#    # since nltk may have just been install
-#    # we need to update our PYTHONPATH
-#    #import site
-#    #reload(site)
-#    ## Now we can import nltk
-#    #import nltk
-#    #import os
-#    #path_to_nltk_f = nltk.__file__
-#    #nltkpath = os.path.dirname(path_to_nltk_f)
-#    #punktpath = os.path.join(nltkpath, 'tokenizers')
-#    #wordnetpath = os.path.join(nltkpath, 'corpora')
-#    #if not os.path.isfile(os.path.join(punktpath, 'punkt.zip')) \
-#    #    and not os.path.isdir(os.path.join(punktpath, 'punkt')):
-#    #    nltk.download('punkt', download_dir = nltkpath)
-#    #if not os.path.isfile(os.path.join(wordnetpath, 'wordnet.zip')) \
-#    #    and not os.path.isdir(os.path.join(wordnetpath, 'wordnet')):
-#    #    nltk.download('wordnet', download_dir = nltkpath)
-#    #nltk.data.path.append(nltkpath)
-#
-#    install_with_pip('corenlp_xml', 'git+git://github.com/interrogator/corenlp-xml-lib.git')
-#    install_with_pip('tkintertable', 'git+git://github.com/interrogator/tkintertable.git')
-#    install_with_pip('PIL', 'http://effbot.org/media/downloads/Imaging-1.1.7.tar.gz')
-#
-#class my_install(install):  
-#    def run(self):
-#        _install.run(self)
-#
-#        # the second parameter, [], can be replaced with a set of parameters if _post_install needs any
-#        self.execute(_post_install, [],  
-#                     msg="Installing NLTK data, etc.")
-#
-#class my_develop(develop):  
-#    def run(self):
-#        self.execute(noop, (self.install_lib,),
-#                     msg="Installing ... ")
-#        _develop.run(self)
-#        self.execute(_post_install, [],
-#                     msg="Installing NLTK data, etc.")
+class CustomInstallCommand(install):
+    """Customized setuptools install command - prints a friendly greeting."""
+    def run(self):
+
+        def install_with_pip(package_name, package):
+            import importlib
+            try:
+                importlib.import_module(package_name)
+            except ImportError:
+                import pip
+                pip.main(['install', package])
+
+        install.run(self)
+        install_with_pip('corenlp_xml', 'git+git://github.com/interrogator/corenlp-xml-lib.git')
+        install_with_pip('tkintertable', 'git+git://github.com/interrogator/tkintertable.git')
+        install_with_pip('PIL', 'http://effbot.org/media/downloads/Imaging-1.1.7.tar.gz')
+
+        # since nltk may have just been install
+        # we need to update our PYTHONPATH
+        import site
+        reload(site)
+        # Now we can import nltk
+        import nltk
+        import os
+        path_to_nltk_f = nltk.__file__
+        nltkpath = os.path.dirname(path_to_nltk_f)
+        punktpath = os.path.join(nltkpath, 'tokenizers')
+        wordnetpath = os.path.join(nltkpath, 'corpora')
+        if not os.path.isfile(os.path.join(punktpath, 'punkt.zip')) \
+            and not os.path.isdir(os.path.join(punktpath, 'punkt')):
+            nltk.download('punkt', download_dir = nltkpath)
+        if not os.path.isfile(os.path.join(wordnetpath, 'wordnet.zip')) \
+            and not os.path.isdir(os.path.join(wordnetpath, 'wordnet')):
+            nltk.download('wordnet', download_dir = nltkpath)
+        nltk.data.path.append(nltkpath)
 
 setup(name='corpkit',
     version='1.62',
@@ -63,10 +47,9 @@ setup(name='corpkit',
                     'dictionaries': ['*.p', 'dictionaries/*.p']},
     author_email='mcdonaldd@unimelb.edu.au',
     license='MIT',
-    #cmdclass={'install': my_install,  # override install
-    #              'develop': my_develop},
+    cmdclass={'install': CustomInstallCommand,
     packages=['corpkit', 'dictionaries'],
-    install_requires=["matplotlib >= 1.4.3",
+    install_requires=[  "matplotlib >= 1.4.3",
                         "nltk >= 3.0.0",
                         "joblib",
                         "pandas >= 0.16.1",
@@ -74,6 +57,7 @@ setup(name='corpkit',
                         "beautifulsoup4>=4.3.2",
                         "lxml>=3.4.4",
                         "blessings>=1.6"],
-                        # PIL, Pillow
-                        #"tkintertable>=1.2"],
+                        # chardet
+                        # requests
+                        # ...
     zip_safe = False)
