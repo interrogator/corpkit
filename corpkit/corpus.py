86f0fe73720aa190472d76ccf2ed72f81b6a3026
daniel <mcdonaldd@unimelb.edu.au>
2016-01-08 16:41:10 +0100

    subcorpus class, etc
    Former-commit-id: 2d025764eadc5d8fa4bc8a52cc63566d9bf47c18 [formerly 0dd04915b793453c01af36ea511e0e548915968f]
    Former-commit-id: 6a3896e379b27d115edaa44b8fbaf03d14304f0b

--- corpkit/corpus.py
+++ corpkit/corpus.py
@@ -1,35 +1,53 @@
-
 class Corpus:
     """A class representing a linguistic text corpus"""
 
-    def __init__(self, path):    
+    def __init__(self, path):
+
         import os
-        from corpkit.build import get_filepaths
         from corpkit.process import determine_datatype
         
-        def get_subcorpora():
-            """get names of subfolders"""
+        def get_structure():
+
+            """print structure of corpus and build .subcorpora, .files and .structure"""
             import os
-            subcorpora = []
-            for root, dirs, fs in os.walk(self.path):
-                if root not in subcorpora and root != self.path:
-                    subcorpora.append(root)
-            return subcorpora
+            from corpkit.corpus import Subcorpus
+            strep = '\nCorpus: %s\n' % self.abspath
+            structdict = {}
+            for dirname, subdirlist, filelist in os.walk(self.path):
+                if os.path.abspath(dirname) == os.path.abspath(self.path):
+                    continue
+                structdict[dirname] = filelist
+                strep = strep + '\nSubcorpus: %s' % os.path.basename(dirname)
+                for fname in filelist:
+                    strep = strep + '\n\t%s' % fname
+            print strep
+            filelist = []
+            for fl in structdict.values():
+                for f in fl:
+                    filelist.append(f)
+            subcs = [Subcorpus(i) for i in structdict.keys()]
+            return structdict, subcs, filelist
 
         self.path = path
         self.abspath = os.path.abspath(path)
-        self.files = get_filepaths(path, ext = False)
         self.datatype, self.singlefile = determine_datatype(path)
-        self.subcorpora = get_subcorpora()
+        self.structure, self.subcorpora, self.files = get_structure()
         self.features = False
 
     def __str__(self):
         """string representation of corpus"""
         st = 'Corpus at %s:\n\nData type: %s\nNumber of subcorpora: %d\n' \
              'Number of files: %d\n' % (self.abspath, self.datatype, len(self.subcorpora), len(self.files))
+        if self.singlefile:
+            st = st + '\nCorpus is a single file.\n'
         if self.features is not False:
-            cols = list(self.features.columns)[:10]
-            st = st + '\nFeatures:\n\n' + self.features.head(10).to_string(columns = cols)
+            if not self.singlefile:
+                cols = list(self.features.columns)[:10]
+                st = st + '\nFeatures:\n\n' + self.features.head(10).to_string(columns = cols)
+            else:
+                st = st + '\nFeatures:\n\n' + self.features.head(10).to_string()
+        else:
+            st = st + '\nFeatures not analysed yet. Use .get_stats() method.\n'
         return st
 
     # METHODS
@@ -37,9 +55,33 @@ class Corpus:
         """get some basic stats"""
         from corpkit import interrogator
         self.features = interrogator(c.path, 's', 'any').results
-        print 'Features defined. See .features attribute ...'
+        print 'Features defined. See .features attribute ...' 
 
     def interrogate(self, *args, **kwargs):
         """interrogate the corpus using corpkit.interrogator.interrogator"""
         from corpkit import interrogator
         return interrogator(self.path, *args, **kwargs)
+
+    def parse(self, *args, **kwargs):
+        from corpkit import make_corpus
+        from corpkit.corpus import Corpus
+        outpaths = make_corpus(self.path)
+        corpora = []
+        for p in outpaths:
+            corpora.append(Corpus(p))
+        return corpora
+
+class Subcorpus(str):
+    
+    def __init__(self, path):
+        self.path = path
+
+    def interrogate(self, *args, **kwargs):
+        """interrogate the corpus using corpkit.interrogator.interrogator()"""
+        from corpkit import interrogator
+        return interrogator(self.path, *args, **kwargs)
+
+    def concordance(self, *args, **kwargs):
+        """interrogate the corpus using corpkit.conc.conc()"""
+        from corpkit import conc
+        return conc(self.path, *args, **kwargs)
