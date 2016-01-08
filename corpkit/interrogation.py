9b32d593a90e224e0c3623ab21c4b760c041f731
daniel <mcdonaldd@unimelb.edu.au>
2016-01-08 17:05:09 +0100

    results and totals branches
    Former-commit-id: 3cfb03fbecb06ab692a0f877f876e4b18cad7a8d [formerly b4cf52d71751bed76bc17d342a9dff9f1e9affc8]
    Former-commit-id: 25f0b680bee3d09436c5d4596034470f9f4f85ea

--- corpkit/interrogation.py
+++ corpkit/interrogation.py
@@ -8,7 +8,7 @@ class Interrogation:
     .. py:method:: edit(*args, **kwargs)
     .. py:method:: plot(title, **kwargs)
     """
-    
+
     def __init__(self, results = None, totals = None, query = None):
         """initialise the class"""
         self.results = results
@@ -17,7 +17,7 @@ class Interrogation:
 
 
     def __str__(self):
-        st = 'Corpus interrogation %s:\n\nData type: %s\n' % (self.path, self.datatype)
+        st = 'Corpus interrogation: %s\n\n' % (self.query['path'])
         return st
 
 
@@ -44,3 +44,37 @@ class Interrogation:
             plotter(title, self.results, *args, **kwargs)
         elif branch.lower().startswith('t'):
             plotter(title, self.totals, *args, **kwargs)
+
+import pandas as pd
+class Results(pd.core.frame.DataFrame):
+
+    def __init__(self, data):
+        pd.core.frame.DataFrame.__init__(self, data)
+
+    def edit(self, *args, **kwargs):
+        """calls corpkit.editor.editor()"""
+        from corpkit import editor
+        return editor(self, *args, **kwargs)
+
+    def plot(self, title, *args, **kwargs):
+        """calls corpkit.plotter.plotter()"""
+        from corpkit import plotter
+        plotter(title, self, *args, **kwargs)
+
+
+class Totals(pd.core.series.Series):
+
+    def __init__(self, data):
+        pd.core.series.Series.__init__(self, data)
+
+    def edit(self, *args, **kwargs):
+        """calls corpkit.editor.editor()"""
+        from corpkit import editor
+        return editor(self, *args, **kwargs)
+
+    def plot(self, title, *args, **kwargs):
+        """calls corpkit.plotter.plotter()"""
+        from corpkit import plotter
+        plotter(title, self, *args, **kwargs)
+
+
