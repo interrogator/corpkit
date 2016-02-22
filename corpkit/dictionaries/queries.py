302e9e22769b848b22783ed4b5a84e642a55bc6b
daniel <mcdonaldd@unimelb.edu.au>
2016-02-22 13:30:06 +0100

    systemic query dicts

---
+++ corpkit/dictionaries/queries.py
@@ -0,0 +1,17 @@
+import corpkit
+from lazyprop import lazyprop
+
+class Queries(object):
+
+    def __init__(self):
+        import corpkit
+        from dictionaries import roles, processes
+        self.sayer = {'f': roles.participant1, 'gl': processes.verbal.lemmata, 'gf': roles.event}
+        self.verbiage = {'f': roles.participant2, 'gl': processes.verbal.lemmata, 'gf': roles.event}
+        self.senser = {'f': roles.participant1, 'gl': processes.mental.lemmata, 'gf': roles.event}
+        self.phenomenon = {'f': roles.participant2, 'gl': processes.mental.lemmata, 'gf': roles.event}
+        self.token = {'f': roles.participant1, 'gl': processes.relational.lemmata, 'gf': roles.event}
+        self.value = {'f': roles.participant2, 'gl': processes.relational.lemmata, 'gf': roles.event}
+        self.agent = {'f': roles.participant1}
+
+queries = Queries()
