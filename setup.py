c4c1f31af085dfe233be5fed4c8167ecd6b0a93c
daniel <mcdonaldd@unimelb.edu.au>
2016-06-17 18:19:19 +0200

    maybe fixing install problems: corenlp_xml can use master

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ import setuptools
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 import os
+from os.path import isfile, isdir, join, dirname
 
 class CustomInstallCommand(install):
     """Customized setuptools install command."""
     def run(self):
+        from setuptools.command.install import install
         install.run(self)
         # since nltk may have just been installed
         # we need to update our PYTHONPATH
@@ -17,40 +19,43 @@ class CustomInstallCommand(install):
         # Now we can import nltk
         import nltk
         path_to_nltk_f = nltk.__file__
-        nltkpath = os.path.dirname(path_to_nltk_f)
-        punktpath = os.path.join(nltkpath, 'tokenizers')
-        wordnetpath = os.path.join(nltkpath, 'corpora')
-        if not os.path.isfile(os.path.join(punktpath, 'punkt.zip')) \
-            and not os.path.isdir(os.path.join(punktpath, 'punkt')):
+        nltkpath = dirname(path_to_nltk_f)
+        punktpath = join(nltkpath, 'tokenizers')
+        wordnetpath = join(nltkpath, 'corpora')
+        if not isfile(join(punktpath, 'punkt.zip')) \
+            and not isdir(join(punktpath, 'punkt')):
             nltk.download('punkt', download_dir=nltkpath)
-        if not os.path.isfile(os.path.join(wordnetpath, 'wordnet.zip')) \
-            and not os.path.isdir(os.path.join(wordnetpath, 'wordnet')):
+        if not isfile(join(wordnetpath, 'wordnet.zip')) \
+            and not isdir(join(wordnetpath, 'wordnet')):
             nltk.download('wordnet', download_dir=nltkpath)
         nltk.data.path.append(nltkpath)
 
 setup(name='corpkit',
-    version='2.1.9',
-    description='A toolkit for working with linguistic corpora',
-    url='http://github.com/interrogator/corpkit',
-    author='Daniel McDonald',
-    packages=['corpkit'],
-    scripts=['corpkit/new_project'],
-    package_dir={'corpkit': 'corpkit'},
-    package_data={'corpkit': ['*.jar', 'corpkit/*.jar', '*.sh', 'corpkit/*.sh', 
-                                '*.ipynb', 'corpkit/*.ipynb', '*.p', 'dictionaries/*.p', '*.py', 'dictionaries/*.py']},
-    author_email='mcdonaldd@unimelb.edu.au',
-    license='MIT',
-    cmdclass={'install': CustomInstallCommand,},
-    keywords = ['corpus', 'linguistics', 'nlp'],
-    install_requires=["matplotlib >= 1.4.3",
-                        "nltk >= 3.0.0",
+      version='2.1.9',
+      description='A toolkit for working with linguistic corpora',
+      url='http://github.com/interrogator/corpkit',
+      author='Daniel McDonald',
+      packages=['corpkit'],
+      scripts=['corpkit/new_project'],
+      package_dir={'corpkit': 'corpkit'},
+      package_data={'corpkit': ['*.jar', 'corpkit/*.jar', '*.sh', 'corpkit/*.sh', 
+                                '*.ipynb', 'corpkit/*.ipynb', '*.p', 'dictionaries/*.p',
+                                '*.py', 'dictionaries/*.py']},
+      author_email='mcdonaldd@unimelb.edu.au',
+      license='MIT',
+      cmdclass={'install': CustomInstallCommand,},
+      keywords=['corpus', 'linguistics', 'nlp'],
+      install_requires=["matplotlib>=1.4.3",
+                        "nltk>=3.0.0",
                         "joblib",
-                        "pandas >= 0.16.1",
-                        "mpld3 >= 0.2", 
+                        "pandas>=0.16.1",
+                        "mpld3>=0.2", 
                         "lxml>=3.4.4",
                         "requests",
                         "chardet",
                         "blessings>=1.6",
-                        "traitlets>=4.1.0"],
-    dependency_links=['git+https://github.com/interrogator/corenlp-xml-lib.git@26f126f33fbf67a17f9a5d5913b17fe99742c8c3#egg=corenlp_xml',
-                      'git+https://github.com/interrogator/tkintertable.git@e983dea6565d583439cbe04034774944388213ae#egg=tkintertable'])
+                        "traitlets>=4.1.0",
+                        "corenlp-xml>=1.0.0"],
+      dependency_links=['git+https://github.com/interrogator/corenlp-xml-lib.git@26f126f33fbf67a17f9a5d5913b17fe99742c8c3#egg=corenlp_xml',
+                        'git+https://github.com/interrogator/tkintertable.git@e983dea6565d583439cbe04034774944388213ae#egg=tkintertable'])
+  
