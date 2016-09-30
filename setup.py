6c94680797c4c111aebfe442f601c57988e23f70
Daniel McDonald <mcdonaldd@unimelb.edu.au>
2016-09-30 19:56:12 +0200

    new version

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ class CustomInstallCommand(install):
         nltk.data.path.append(nltkpath)
 
 setup(name='corpkit',
-      version='2.3.1',
+      version='2.3.2',
       description='A toolkit for working with linguistic corpora',
       url='http://github.com/interrogator/corpkit',
       author='Daniel McDonald',
