64f12fc42c4348254809a5f73f4c1dcc2c471f61
daniel <mcdonaldd@unimelb.edu.au>
2016-12-09 22:59:30 +0100

    feature/postags/wordlcasses can be series

--- corpkit/corpus.py
+++ corpkit/corpus.py
@@ -353,7 +353,10 @@ class Corpus(object):
 
         if name in md:
             import pandas as pd
-            return pd.DataFrame(md[name])
+            try:
+                return pd.DataFrame(md[name])
+            except ValueError:
+                return pd.Series(md[name])
         else:
             feat = self.interrogate('features', **kwa)
             from corpkit.interrogation import Interrodict
@@ -381,7 +384,11 @@ class Corpus(object):
         
         if pname in md and wname in md:
             import pandas as pd
-            return pd.DataFrame(md[pname]), pd.DataFrame(md[wname])
+            try:
+                return pd.DataFrame(md[pname]), pd.DataFrame(md[wname])
+            except ValueError:
+                return pd.Series(md[pname]), pd.Series(md[wname])
+
         else:
             postags = self.interrogate('postags', **kwa)
             from corpkit.interrogation import Interrodict
